2ae4665bc901b8a4b708b0324a131fd1121c1270
gandhi56 <gandhi21299@gmail.com>
2020-03-10 18:17:10 -0600

    1.5 questions done

--- assign3/a.pl
+++ assign3/a.pl
@@ -1,8 +1,8 @@
 a3last([X], X).
 a3last([_|Rest], Last) :- 
-  a3last(Rest, RestLast), 
-  write(Rest),
-  Last is RestLast.
+  a3last(Rest, Last).
 
+is_even(N) :- mod(N, 2) =:= 0.
 
+%even(L, E)
 
