9b3b97699f1343a67a9cb7dc91757fb99d9502ab
gandhi56 <gandhi21299@gmail.com>
2020-04-02 22:20:12 -0600

    fixed a1.pl

--- assign4/a1.pl
+++ assign4/a1.pl
@@ -1,27 +1,26 @@
 
 menu(M) :-
   length(M, 7),
-  ok(M, 7).
+  ok(M, 7, 0).
 
-ok([H|Rest], 7) :- fish_dish(H), ok(Rest, 6), \+ member(H, Rest).
-ok([H|Rest], 6) :- fish_dish(H), ok(Rest, 5), \+ member(H, Rest).
-ok([H|Rest], 6) :- meat_dish(H), ok(Rest, 5), \+ member(H, Rest).
-ok([H|Rest], 5) :- fish_dish(H), ok(Rest, 4), \+ member(H, Rest).
-ok([H|Rest], 5) :- vegetarian_dish(H), ok(Rest, 4), \+ member(H, Rest).
-ok([H|Rest], 4) :- meat_dish(H), ok(Rest, 3), \+ member(H, Rest).
-ok([H|Rest], 4) :- vegetarian_dish(H), ok(Rest, 3), \+ member(H, Rest).
-ok([H|Rest], 3) :- vegetarian_dish(H), ok(Rest, 2), \+ member(H, Rest).
-ok([H|Rest], 2) :- vegetarian_dish(H), ok(Rest, 1), \+ member(H, Rest).
-ok([H|Rest], 2) :- meat_dish(H), ok(Rest, 1), \+ member(H, Rest).
-ok([H], 1) :-   meat_dish(H).
+ok([H|Rest], 7, X) :- fish_dish(H), ok(Rest, 6, X), \+ member(H, Rest).
+ok([H|Rest], 6, X) :- fish_dish(H), ok(Rest, 5, X), \+ member(H, Rest).
+ok([H|Rest], 6, X) :- meat_dish(H), ok(Rest, 5, X), \+ member(H, Rest), X = 0.
+ok([H|Rest], 5, X) :- fish_dish(H), ok(Rest, 4, X), \+ member(H, Rest).
+ok([H|Rest], 5, X) :- vegetarian_dish(H), ok(Rest, 4, X), \+ member(H, Rest).
+ok([H|Rest], 4, X) :- meat_dish(H), ok(Rest, 3, X), \+ member(H, Rest), X = 0.
+ok([H|Rest], 4, X) :- vegetarian_dish(H), ok(Rest, 3, X), \+ member(H, Rest).
+ok([H|Rest], 3, X) :- vegetarian_dish(H), ok(Rest, 2, X), \+ member(H, Rest).
+ok([H|Rest], 2, X) :- vegetarian_dish(H), ok(Rest, 1, X), \+ member(H, Rest).
+ok([H|Rest], 2, X) :- meat_dish(H), ok(Rest, 1, X), \+ member(H, Rest), X = 0.
+ok([H], 1, 0) :- meat_dish(H).
+ok([H], 1, 1) :- vegetarian_dish(H).
+ok([H], 1, 1) :- fish_dish(H).
 
 count_solutions(C) :-   aggregate_all(count, menu(_), C).
 
 meat_free_menu(M) :-
-  menu(M),
-  no_meat(M).
-
-no_meat([H]) :- meat_dish(H).
-no_meat([H|Rest]) :- \+ meat_dish(H), no_meat(Rest).
+  length(M, 7),
+  ok(M, 7, 1).
 
 count_meat_free_solutions(C) :-   aggregate_all(count, meat_free_menu(_), C).
